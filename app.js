fc214261f607c2b2e382ea8127d7d930502254df
Stanley <stanley.traub@gmail.com>
2019-12-24 13:29:50 -0800

    Fixed reviews

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 const express = require("express");
+const path = require('path');
 const app = express();
-const db = require("./config/keys").mongoURI;
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require("./routes/api/users");
-// const tweets = require("./routes/api/tweets");
+const reviews = require("./routes/api/reviews");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
+  .then(() => console.log("Connected to MongoDB sucessfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!!"));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('frontend/build'));
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   })
+// }
+
 
 app.use(passport.initialize());
-// require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/users", users);
-// app.use("/api/tweets", tweets);
+app.use("/api/reviews", reviews);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
