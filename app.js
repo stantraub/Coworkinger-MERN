1ea0a2dea1d869184250884b852f381887db2f07
Stanley <stanley.traub@gmail.com>
2019-12-25 09:28:52 -0800

    Set up Sign up and Login components

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ mongoose
   .then(() => console.log("Connected to MongoDB sucessfully"))
   .catch(err => console.log(err));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('frontend/build'));
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   })
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 
 app.use(passport.initialize());
---
+++ frontend/src/components/main/main_page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+    render() {
+        return (
+          <div>
+            <h1>Coworking Space Reviews</h1>
+            <footer>Copyright &copy; 2019 Coworking Inc.</footer>
+          </div>
+        );
+    }
+}
+
+export default MainPage;
