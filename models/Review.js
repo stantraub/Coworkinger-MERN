a6f17a3d3b36a3756394fef5fdd3a6281804ce8b
Stanley <stanley.traub@gmail.com>
2020-03-21 20:34:15 -0700

    Fixed MongoDB issue

--- models/Review.js
+++ models/Review.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
-    user: {
+    reviewer: {
         type: Schema.Types.ObjectId,
         ref: 'users'
     },
--- models/Space.js
+++ models/Space.js
@@ -10,11 +10,23 @@ const SpaceSchema = new Schema({
         type: String,
         required: true
     },
-    email: {
+    city: {
         type: String,
         required: true
     },
-    phone: {
+    state: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    email: {
         type: String,
         required: true
     },
@@ -22,9 +34,121 @@ const SpaceSchema = new Schema({
         type: Number,
         required: true
     },
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
+    phone: {
+        type: String,
+        required: true
+    },
+    website: {
+        type: String,
+        required: false
+    },
+    openHour: {
+        type: String,
+        required: true
+    },
+    closingHour: {
+        type: String,
+        required: true
+    },
+    neighborhood: {
+        type: String,
+        required: false
+    },
+    officeCapacity: {
+        type: Number,
+        required: false
+    },
+    peopleCapacity: {
+        type: Number,
+        required: false
+    },
+    availability: {
+        type: Boolean,
+        required: false
+    },
+    deskDay: {
+        type: Boolean,
+        required: false
+    },
+    sharedDesk: {
+        type: Number,
+        required: false
+    },
+    meetingRooms: {
+        type: Number,
+        required: false
+    },
+    hours24Access: {
+        type: Boolean,
+        required: false
+    },
+    transitStationMiles: {
+        type: Number,
+        required: false
+    },
+    wellnessRoom: {
+        type: Boolean,
+        required: false
+    },
+    phoneBooths: {
+        type: Number,
+        required: false
+    },
+    eventSpace: {
+        type: Boolean,
+        required: false
+    },
+    bikeParking: {
+        type: Boolean,
+        required: false
+    },
+    petFriendly: {
+        type: Boolean,
+        required: false
+    },
+    snacksDrinksIncluded: {
+        type: Boolean,
+        required: false
+    },
+    teaCoffeeIncluded: {
+        type: Boolean,
+        required: false
+    },
+    pingPong: {
+        type: Boolean,
+        required: false
+    },
+    billiards: {
+        type: Boolean,
+        required: false
+    },
+    foosball: {
+        type: Boolean,
+        required: false
+    },
+    showers: {
+        type: Boolean,
+        required: false
+    },
+    onsiteGym: {
+        type: Boolean,
+        required: false
+    },
+    printersIncluded: {
+        type: Boolean,
+        required: false
+    },
+    bocceBall: {
+        type: Boolean,
+        required: false
+    },
+    napRoom: {
+        type: Boolean,
+        required: false
+    },
+    mainPhoto: {
+        type: String,
+        required: false
     }
 });
 
