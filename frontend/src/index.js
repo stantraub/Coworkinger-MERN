b73cc0d9351476a26fe3aa0b08feb36b7ddaef60
Stanley <stanley.traub@gmail.com>
2020-03-22 12:47:41 -0700

    Fixed local storage property typo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ import axios from 'axios';
 document.addEventListener('DOMContentLoaded', () => {
     let store;
 
-    if (localStorage.jwt_decode) {
+    if (localStorage.jwtToken) {
         setAuthToken(localStorage.jwtToken);
         const decodedUser = jwt_decode(localStorage.jwtToken);
         const preloadedState = { session: { isAuthenticated: true, user: decodedUser}};
