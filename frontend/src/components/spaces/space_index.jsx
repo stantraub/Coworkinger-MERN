735a2ce2e0274128a7fad5da5c03f1946632cf19
Stanley <stanley.traub@gmail.com>
2020-04-04 15:24:58 -0700

    Finished up map

--- frontend/src/components/spaces/space_index.jsx
+++ frontend/src/components/spaces/space_index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import SpaceItem from './space_item';
 import './space_index.css'
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
@@ -12,74 +12,89 @@ import {
 } from "react-device-detect";
 
 const Spaces = props => {
+  const [activeSpace, setActiveSpace] = useState(null)
+  useEffect(() => {
+      props.fetchSpaces()
+  }, [])
 
-    useEffect(() => {
-        props.fetchSpaces()
-    }, [])
-
-    if (isBrowser) {
-        if (props.spaces.length === 0) {
-            return <div>There are no spaces in this location</div>;
-        } else {
-            const { spaces } = props
-            return (
-              <div className="space-index-main">
-                <div className="spaces-index-spaces-wrapper">
-                  <div className="space-index-header">
-                    All spaces in San Francisco
-                  </div>
-                  {spaces.map(({ _id, ...otherSpaceProps }) => {
-                    return (
-                      <SpaceItem key={_id} spaceId={_id} {...otherSpaceProps} />
-                    );
-                  })}
+  if (isBrowser) {
+      if (props.spaces.length === 0) {
+          return <div>There are no spaces in this location</div>;
+      } else {
+          const { spaces } = props
+          return (
+            <div className="space-index-main">
+              <div className="spaces-index-spaces-wrapper">
+                <div className="space-index-header">
+                  Coworking spaces in San Francisco
                 </div>
-                <div className="space-index-map">
-                  <Map center={[37.773943, -122.449484]} zoom={13.4}>
-                    <TileLayer
-                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    />
-
-                {spaces.map(space => {
-                  
+                {spaces.map(({ _id, ...otherSpaceProps }) => {
                   return (
-                    <Marker
-                      key={space._id}
-                      position={[
-                        space.latitude,
-                        space.longitude
-                      ]}
-                    />
-                  )
-                  
-                  })}
-                    
-                  </Map>
-                </div>
+                    <SpaceItem key={_id} spaceId={_id} {...otherSpaceProps} />
+                  );
+                })}
               </div>
-            );
-        }
-    } else {
-        if (props.spaces.length === 0) {
-            return <div>There are no spaces in this location</div>;
-        } else {
-            const { spaces } = props
+              <div className="space-index-map">
+                <Map center={[37.773943, -122.449484]} zoom={13.4}>
+                  <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  />
 
-            return (
-                <div className="space-index-main-mobile">
-                    <h1 className="space-index-header-mobile">
-                        All San Francisco workspaces
-              </h1>
-                    <div className="spaces-index-wrapper">
-                        {spaces.map(({ _id, ...otherSpaceProps }) => {
-                            return <SpaceItem key={_id} spaceId={_id} {...otherSpaceProps} />;
-                        })}
-                    </div>
-                </div>
-            );
-        }
-    }
+                  {spaces.map(space => (
+                      <Marker
+                        key={space._id}
+                        position={[
+                          space.latitude,
+                          space.longitude
+                        ]}
+                        onClick={() => setActiveSpace(space)}
+                      />
+
+                      
+                  ))}
+
+                  {activeSpace && (
+                    <Popup
+                      position = {[
+                        activeSpace.latitude,
+                        activeSpace.longitude
+                      ]}
+                      onClose={() => setActiveSpace(null)}
+                    >
+                      <div>
+                        <h2>{activeSpace.name}</h2>
+                        <img className="main-pic" src={activeSpace.mainPhoto}></img>
+                        <span className="popup-space-description"><strong>${activeSpace.cost}</strong> per desk / month</span>
+                      </div>
+                    </Popup>
+                  )}
+                  
+                </Map>
+              </div>
+            </div>
+          );
+      }
+  } else {
+      if (props.spaces.length === 0) {
+          return <div>There are no spaces in this location</div>;
+      } else {
+          const { spaces } = props
+
+          return (
+              <div className="space-index-main-mobile">
+                  <h1 className="space-index-header-mobile">
+                      All San Francisco workspaces
+            </h1>
+                  <div className="spaces-index-wrapper">
+                      {spaces.map(({ _id, ...otherSpaceProps }) => {
+                          return <SpaceItem key={_id} spaceId={_id} {...otherSpaceProps} />;
+                      })}
+                  </div>
+              </div>
+          );
+      }
+  }
 }
 
 export default Spaces
