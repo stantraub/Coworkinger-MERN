0bb624daea56c2ff28989349b0cafa821aed81dc
Stanley <stanley.traub@gmail.com>
2020-04-13 19:56:43 -0700

    heroku test

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  secretOrKey: process.env.SECRET_OR_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  region: process.env.REGION,
+  bucket: process.env.BUCKET
 }
