df13ea7a9fb31f59b46870f6d555e964d791531a
Stanley <stanley.traub@gmail.com>
2020-04-27 20:12:31 -0700

    Started user reviews container

--- frontend/src/components/user/user_reviews_container.js
+++ frontend/src/components/user/user_reviews_container.js
@@ -2,11 +2,16 @@ import UserReviews from "./user_reviews"
 import { connect } from 'react-redux'
 
 const msp = state => {
-
+    const { id: userId } = state.session.user
+    return {
+        userId
+    }
 }
 
 const mdp = dispatch => {
 
 }
+
+
 
 export default connect(msp, mdp)(UserReviews)
