10f52e1f9ba3bb50bb4ef0544e39caed8d057700
Stanley <stanley.traub@gmail.com>
2020-06-27 15:10:28 -0700

    Start picture grid

--- client/src/components/spaces/pictures-grid.jsx
+++ client/src/components/spaces/pictures-grid.jsx
@@ -6,42 +6,41 @@ const PicturesGrid = (props) => {
     return (
       <div className="pictures">
         <div className="pictures__main-container">
-          <button
-            className="photos-btn-main-pic"
-          >
-            View Photos
-          </button>
           <img className="space-show-pic-image" src={photos[0]} alt="main" />
         </div>
-        <div className="space-show-space-pics">
-          <div className="space-show-pic-column">
-            <img
-              className="space-show-pic-image"
-              src={photos[1]}
-              alt="first"
-            ></img>
-            <img
-              className="space-show-pic-image"
-              src={photos[2]}
-              alt="second"
-            ></img>
-            <button className="photos-btn-first-column">View Photos</button>
-          </div>
-          <div className="space-show-pic-column">
-            <img
-              className="space-show-pic-image"
-              src={photos[3]}
-              alt="third"
-            ></img>
-            <img
-              className="space-show-pic-image"
-              src={photos[4]}
-              alt="fourth"
-            ></img>
-            <button className="photos-btn">View Photos</button>
-          </div>
+        <div className="pictures__secondary-photo">
+          <img
+            className="pictures__secondary-photo-1"
+            src={photos[1]}
+            alt="first"
+          />
         </div>
-      </div>
+        <div>
+          <img
+            className="pictures__secondary-photo-2"
+            src={photos[2]}
+            alt="second"
+          />
+        </div>
+        <div>
+          <img
+            className="pictures__secondary-photo-2"
+            src={photos[3]}
+            alt="third"
+          />
+        </div>
+        <div>
+          <img
+            className="pictures__secondary-photo-2"
+            src={photos[4]}
+            alt="fourth"
+          />
+        </div>
+ 
+
+
+
+        </div>
     );
 }
 
